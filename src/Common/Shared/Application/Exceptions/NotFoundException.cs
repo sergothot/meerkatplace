e57741eced19450596fce9cf6439350743005065
mmmlll18 <markovnik.polina18@gmail.com>
2026-06-24 15:02:26 +0300

    feat: add DomainException, NotFoundException, and ValidationException classes for improved error handling

---
+++ src/Common/Shared/Application/Exceptions/NotFoundException.cs
@@ -0,0 +1,9 @@
+namespace Common.Shared.Application.Exceptions;
+
+public class NotFoundException : DomainException
+{
+    public NotFoundException(string message)
+        : base(message)
+    {
+    }
+}
