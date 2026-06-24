d917b6834869063f959eb83202ec8007e1e1738c
sergothot <sergot.hot@gmail.com>
2026-06-24 15:18:28 +0300

    feat: add integration tests for checkout flow and enhance unit tests for services

--- tests/Services/ListingService/ListingService.UnitTests/UnitTest1.cs
+++ tests/Services/ListingService/ListingService.UnitTests/UnitTest1.cs
@@ -1,10 +1,41 @@
-﻿namespace ListingService.UnitTests;
+﻿using Common.Shared.Domain.Enums;
+using ListingService.API.Domain.Entities;
+using ListingService.API.Domain.Enums;
 
-public class UnitTest1
+namespace ListingService.UnitTests;
+
+public class ProductDomainTests
 {
     [Fact]
-    public void Test1()
+    public void Create_WithInvalidName_Throws()
     {
+        Assert.Throws<InvalidOperationException>(() =>
+            Product.Create(Guid.NewGuid(), " ", "desc", 10m, Currency.RUB, DeliveryType.Physical));
+    }
 
+    [Fact]
+    public void Create_WithInvalidPrice_Throws()
+    {
+        Assert.Throws<InvalidOperationException>(() =>
+            Product.Create(Guid.NewGuid(), "Book", "desc", 0m, Currency.RUB, DeliveryType.Physical));
+    }
+
+    [Fact]
+    public void Activate_FromDraft_SetsActiveStatus()
+    {
+        var product = Product.Create(Guid.NewGuid(), "Book", "desc", 100m, Currency.RUB, DeliveryType.Physical);
+
+        product.Activate();
+
+        Assert.Equal(ProductStatus.Active, product.Status);
+    }
+
+    [Fact]
+    public void Activate_AfterArchive_Throws()
+    {
+        var product = Product.Create(Guid.NewGuid(), "Book", "desc", 100m, Currency.RUB, DeliveryType.Physical);
+        product.Archive();
+
+        Assert.Throws<InvalidOperationException>(product.Activate);
     }
 }
--- tests/Services/PaymentService/PaymentService.UnitTests/UnitTest1.cs
+++ tests/Services/PaymentService/PaymentService.UnitTests/UnitTest1.cs
@@ -1,10 +1,36 @@
-﻿namespace PaymentService.UnitTests;
+﻿using PaymentService.API.Domain.Entities;
+using PaymentService.API.Domain.Enums;
 
-public class UnitTest1
+namespace PaymentService.UnitTests;
+
+public class PaymentTransactionDomainTests
 {
     [Fact]
-    public void Test1()
+    public void MarkSucceeded_FromPending_Succeeds()
     {
+        var payment = new PaymentTransaction();
 
+        payment.MarkSucceeded();
+
+        Assert.Equal(PaymentStatus.Succeeded, payment.Status);
+    }
+
+    [Fact]
+    public void MarkRefunded_FromSucceeded_Succeeds()
+    {
+        var payment = new PaymentTransaction();
+        payment.MarkSucceeded();
+
+        payment.MarkRefunded();
+
+        Assert.Equal(PaymentStatus.Refunded, payment.Status);
+    }
+
+    [Fact]
+    public void MarkRefunded_FromPending_Throws()
+    {
+        var payment = new PaymentTransaction();
+
+        Assert.Throws<InvalidOperationException>(payment.MarkRefunded);
     }
 }
