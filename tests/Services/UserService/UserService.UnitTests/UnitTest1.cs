f34d9d3f4e15a6e3c5bf15506372f4ae1cfa0027
mmmlll18 <markovnik.polina18@gmail.com>
2026-06-24 15:18:28 +0300

    feat: add integration tests for checkout flow and enhance unit tests for services

--- tests/Services/UserService/UserService.UnitTests/UnitTest1.cs
+++ tests/Services/UserService/UserService.UnitTests/UnitTest1.cs
@@ -1,10 +1,33 @@
-﻿namespace UserService.UnitTests;
+﻿using UserService.API.Domain.Entities;
 
-public class UnitTest1
+namespace UserService.UnitTests;
+
+public class AddressDomainTests
 {
     [Fact]
-    public void Test1()
+    public void Create_WithBlankCity_Throws()
     {
+        Assert.Throws<InvalidOperationException>(() =>
+            Address.Create(Guid.NewGuid(), "RU", " ", "Street", "101000"));
+    }
 
+    [Fact]
+    public void Create_TrimsValues()
+    {
+        var address = Address.Create(Guid.NewGuid(), " RU ", " Moscow ", " Tverskaya ", " 101000 ");
+
+        Assert.Equal("RU", address.Country);
+        Assert.Equal("Moscow", address.City);
+        Assert.Equal("Tverskaya", address.Street);
+        Assert.Equal("101000", address.PostalCode);
+    }
+
+    [Fact]
+    public void Update_WithBlankStreet_Throws()
+    {
+        var address = Address.Create(Guid.NewGuid(), "RU", "Moscow", "Tverskaya", "101000");
+
+        Assert.Throws<InvalidOperationException>(() =>
+            address.Update("RU", "Moscow", " ", "101000"));
     }
 }
