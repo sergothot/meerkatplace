c0493a5a98c08534c7fc1bdf5acbe2c9ded3b980
sergothot <sergot.hot@gmail.com>
2026-06-25 22:40:37 +0300

    feat: implement JWT authentication and user authorization for payment and wallet services

--- src/Services/PaymentService/PaymentService.API/Application/Payments/IPaymentService.cs
+++ src/Services/PaymentService/PaymentService.API/Application/Payments/IPaymentService.cs
@@ -2,9 +2,9 @@ namespace PaymentService.API.Application.Payments;
 
 public interface IPaymentService
 {
-    Task<IResult> CreatePaymentAsync(CreatePaymentRequest request);
+    Task<IResult> CreatePaymentAsync(HttpContext httpContext, CreatePaymentRequest request);
 
     Task<IResult> GetPaymentAsync(Guid paymentId);
 
-    Task<IResult> RefundPaymentAsync(Guid paymentId);
+    Task<IResult> RefundPaymentAsync(HttpContext httpContext, Guid paymentId);
 }
